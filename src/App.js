b0edd2220bd3ab09ad3f45a5bb296eb10250db33
Yurim Park <99720055+yurimpark@users.noreply.github.com>
2023-02-26 13:00:10 +0900

    [수정] 주석 수정

--- src/App.js
+++ src/App.js
@@ -14,8 +14,8 @@ import {
 // 상품 갯수를 늘리거나 줄일 수 있다. (카운트 값이 증가하면 총 수량 및 가격이 업데이트 되어 ui에 반영)
 // 제품 갯수가 0이면 구매하기 버튼이 비활성화(disable) 된다.
 // 구매하기 버튼에 마우스를 올리면 호버 상태(진한색)로 변경된다.
+// 유연한 레이아웃(반응형)
 //todo 도움말 버튼을 클릭하면 툴팁이 표시된다.
-//todo 유연한 레이아웃(반응형)
 
 export default function App() {
   const [counter, setCounter] = useState(1);
