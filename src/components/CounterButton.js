eee31abf296b9ce0fa135cade9c540e725c73b25
Yurim Park <99720055+yurimpark@users.noreply.github.com>
2023-02-26 12:01:50 +0900

    [수정] 카운터 버튼

--- src/components/CounterButton.js
+++ src/components/CounterButton.js
@@ -1,20 +1,34 @@
 import React from "react";
 
-const CounterButton = ({ increase, decrease, counter, btnDisabled }) => {
+const CounterButton = ({
+  increase,
+  decrease,
+  counter,
+  btnDisabled,
+  isDisabled,
+}) => {
   return (
     <div className="counter-container">
       <div className="counter">
         <button
-          disabled={btnDisabled}
           className="counter-btn minus"
+          type="button"
+          aria-label="수량빼기버튼"
+          disabled={btnDisabled}
           onClick={decrease}
+          style={{ cursor: isDisabled ? "not-allowed" : "pointer" }}
         >
           -
         </button>
         <span className="a11y">수량</span>
         <span className="counter-value">{counter}</span>
         <span className="a11y">개</span>
-        <button className="counter-btn plus" onClick={increase}>
+        <button
+          className="counter-btn plus"
+          type="button"
+          aria-label="수량더하기버튼"
+          onClick={increase}
+        >
           +
         </button>
       </div>
